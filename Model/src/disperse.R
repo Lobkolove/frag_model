08e05d931d718cecdecf15682e4abb92dc0c28cf
Julian N. Thiem <julianthiem@gmail.com>
2025-12-08 16:58:13 +0100

    Added toroidal dispersal function, still not used in the model so far.

--- Model/src/disperse.R
+++ Model/src/disperse.R
@@ -26,3 +26,39 @@ disperse <- function(cur_loc, d_sd, d_mean) {
   new_loc <- c(new_x, new_y)
   return(new_loc)
 }
+
+
+######################## Toroidal dispersal ########################################
+
+# Wraps coordinates on a toroidal grid: moves that exit one side re-enter from
+# the opposite side. Keeps the original kernel logic but enforces modular
+# coordinates instead of rejecting off-grid moves.
+toroidal_disperse <- function(cur_loc,
+                              d_sd,
+                              d_mean,
+                              grid_size = mod_par$grid_size) {
+  dis_sd <- d_sd
+  dis_mean <- d_mean
+  
+  if (switch$kernel_type == 0) {
+    sigma <- sqrt(log(1 + (dis_sd * dis_sd) / (dis_mean * dis_mean)))
+    mu <- log(dis_mean) - 0.5 * sigma * sigma
+    distance <- rlnorm(1, mu, sigma)
+  } else if (switch$kernel_type == 1) {
+    distance <- rexp(1, 1 / dis_mean)
+  } else {
+    print("please check switches")
+  }
+  
+  direction <- runif(1, min = 0, max = 2 * pi)
+  
+  raw_x <- cur_loc[1] + round(cos(direction) * distance)
+  raw_y <- cur_loc[2] + round(sin(direction) * distance)
+  
+  # wrap to 1..grid_size
+  new_x <- ((raw_x - 1) %% grid_size) + 1
+  new_y <- ((raw_y - 1) %% grid_size) + 1
+  
+  new_loc <- c(new_x, new_y)
+  return(new_loc)
+}

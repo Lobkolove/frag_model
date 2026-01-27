1e513dcd53f648c363f8a9f07fe6126c14c63867
J. N. Thiem <julianthiem@gmail.com>
2026-01-27 22:09:15 +0100

    Extended scripts for validation and demonstration purposes.

--- scripts/demo_fragmentation.R
+++ scripts/demo_fragmentation.R
@@ -1,6 +1,8 @@
 # Script to test outputs of the functions simulating the fragmentation step.
 library(raster)
 library(data.table)
+library(dplyr)
+library(purrr)
 library(viridis)
 source("Model/parameters.R")
 source("Model/src/initialize.R")
@@ -10,7 +12,10 @@ source("Model/src/generate_agents.R")
 source("Model/src/distribute_agents.R")
 
 source("Model/src/cookie_cutting.R")
-source("Model/src/ls_mask.R")
+source("Model/src/fragmentation.R")
+
+
+# 0. Setup ----------------------------------------------------------------
 
 # Example parameters:
 spatial_ac <- 0.7
@@ -37,48 +42,72 @@ agents       <- model_start$agents
 raster::image(full_grid, asp = 1, col = viridis(100))
 
 
-# Initialize list for fragmented grids
-frag_seq <- list(cookie_cutter = list(),
-                 mask = list())
+# 1. Generate outputs -----------------------------------------------------
 
-# Generate fragmented grids
-for (i in 1:length(frag_levels)) {
+# Initialise results data frame (one row per frag_level)
+results <- tibble(frag_level = frag_levels,
+                  cookie_cutter = list(NULL),
+                  mask = list(NULL))
+
+# Sequential generation
+for (i in seq_along(frag_levels)) {
+
+  # Store all outputs (grid, agents, agents_grid)
   
-  # With cookie cutter function
-  full_ck <- cookie_cutting(
-    grid = full_grid,
-    agents = agents,
-    agents_grid = agents_grid,
-    habitat = habitat_ratio,
-    fragmentation = frag_levels[i]
-  )
-  frag_seq$cookie_cutter[i] <- full_ck$grid
+  results$cookie_cutter[[i]] <- cookie_cutting(grid = full_grid, 
+                                               agents = agents, 
+                                               agents_grid = agents_grid,
+                                               habitat = habitat_ratio, 
+                                               fragmentation = frag_levels[i])
   
-  # With mask function
-  full_ms <- ls_mask(
-    grid = full_grid,
-    agents = agents,
-    agents_grid = agents_grid,
-    habitat = habitat_ratio,
-    fragmentation = frag_levels[i]
-  )
-  frag_seq$mask[i] <- full_ms$grid
+  results$mask[[i]] <- fragment(grid = full_grid, 
+                                agents = agents, 
+                                agents_grid = agents_grid,
+                                habitat = habitat_ratio, 
+                                fragmentation = frag_levels[i],
+                                seed = seed)
 }
 
-# Visualise fragmented grids
+
+# 2. Compare outputs ------------------------------------------------------
+
+  ## 2.1 Compare grids ####
+
 par(mfrow = c(1,2), pty = "s")
-for (i in 1:length(frag_levels)) {
+for (i in seq_along(frag_levels)) {
+  
+  cat("Fragmentation level: ", frag_levels[i], "\n")
+  
+  old_grid <- results$cookie_cutter[[i]]$grid
+  new_grid <- results$mask[[i]]$grid
+  
+  # Grid geometry check
+  if (!raster::compareRaster(old_grid, new_grid, res = TRUE)) {
+    cat("Raster comparison failed!", frag_levels[i], "\n")
+  }
+  
+  # Grid values check  
+  diff_grid <- old_grid != new_grid
+  cat("Grid cells differing:", sum(getValues(diff_grid), na.rm=TRUE), "\n\n")
   
   par(mar = c(2,2,2,1))
-  image(frag_seq$cookie_cutter[[i]], main = "Cookie Cutter",
+  image(old_grid, main = "Cookie Cutter",
         asp = 1, col = viridis(100),
         xaxt = "n", yaxt = "n")
   par(mar = c(2,1,2,2))
-  image(frag_seq$mask[[i]], main = "Mask",
+  image(new_grid, main = "Mask",
         asp = 1, col = viridis(100),
         xaxt = "n", yaxt = "n")
   mtext(paste0("Fragmentation = ", frag_levels[i]), 
         side = 3, line = -3, cex = 1.2, outer = TRUE)
 }
+
+  ## 2.2 Compare agents ####
+
+# ...
+
+  ## 2.3 Compare agents_grid ####
+
+# ...
 
 
--- scripts/demo_spatial_curves.R
+++ scripts/demo_spatial_curves.R
@@ -7,38 +7,38 @@ library(scam)
 
 source(here("R", "dist_decay.R"))
 source(here("R", "sSBR.R"))
-       
+# source(here("R", "toroidal_dist.R"))
 
-# 0. Model sample output --------------------------------------------------
 
-sample_low <- read.csv(here("data-raw/test_Ju_low_fragmentation_output_sample.csv"))
-sample_high <- read.csv(here("data-raw/test_Ju_high_rep_1_output_sample.csv"))
+# 0. Setup ----------------------------------------------------------------
 
-# At the moment, we're only interested in the samples from right after 
-# fragmentation
-low41 <- sample_low %>% 
-  filter(step == 41)
-high41 <- sample_high %>% 
-  filter(step == 41)
+# Read in data with different fragmentation levels and sampling methods
+
+low_rand <- read.csv("data-raw/post_frag_low_sample_ran.csv")
+low_full <- read.csv("data-raw/post_frag_low_sample_full.csv")
+low_cb <- read.csv("data-raw/post_frag_low_sample_cb.csv")
+
+high_rand <- read.csv("data-raw/post_frag_high_sample_ran.csv")
+high_full <- read.csv("data-raw/post_frag_high_sample_full.csv")
+high_cb <- read.csv("data-raw/post_frag_high_sample_cb.csv")
 
 
 # 1. Distance decay -------------------------------------------------------
-
+  
 # Compute distance decays
-dd_low <- dist_decay(low41,
-                     binary = FALSE,
-                     method = "bray")
-dd_high <- dist_decay(high41,
-                      binary = FALSE,
-                      method = "bray")
+dd_low <- dist_decay(low_rand, method = "bray")
+dd_high <- dist_decay(high_rand, method = "bray")
 
 # Plot distance decays
+par(mfrow = c(1, 2))
 plot(dd_low, col = "darkslategrey")
 mtext("Low fragmentation", side = 3, line = 0.5, 
       cex = 0.9, col = "darkslategrey")
 plot(dd_high, col = "steelblue4")
 mtext("High fragmentation", side = 3, line = 0.5, 
       cex = 0.9, col = "steelblue4")
+  
+
 
 
 # 2. Spatially constrained sample based rarefaction (sSBR) ----------------
@@ -93,9 +93,7 @@ mtext("High fragmentation", side = 3, line = 0.5,
 
 # Example with different sampling methods ---------------------------------
 
-sample_ran <- read.csv("data-raw/post_frag_sample_ran.csv")
-sample_full <- read.csv("data-raw/post_frag_sample_full.csv")
-sample_cb <- read.csv("data-raw/post_frag_sample_cb.csv")
+
 
 # Compute distance decays
 dd_ran <- dist_decay(sample_ran, binary = FALSE, method = "bray")
@@ -124,5 +122,58 @@ mtext("Full grid", side = 3, line = 0.5, cex = 0.9, col = "steelblue4")
 plot(src_cb, col = "violetred4")
 mtext("Chessboard sampling", side = 3, line = 0.5, cex = 0.9, col = "violetred4")
 
+
+# Full iterative ----------------------------------------------------------
+
+# Read in data
+sampling_methods <- c("ran", "full", "cb")
+fragmentation_levels <- c("low", "high")
+
+data <- list()
+
+for (frag in fragmentation_levels) {
+  for (samp in sampling_methods) {
+    key <- paste(frag, samp, sep = "_")
+    file <- paste0("data-raw/post_frag_", frag, "_sample_", samp, ".csv")
+    data[[key]] <- read.csv(file)
+  }
+}
+
+# Compute distance decays
+dd <- list()
+for (samp in sampling_methods) {
+  dd[[paste0("low_", samp)]]  <- dist_decay(data[[paste0("low_", samp)]],  method = "bray")
+  dd[[paste0("high_", samp)]] <- dist_decay(data[[paste0("high_", samp)]], method = "bray")
+}
+
+# Plot curves
+cols <- c(low = "darkslategrey", high = "steelblue4")
+
+for (samp in sampling_methods) {
+  
+  par(mfrow = c(1, 2), mar = c(4, 4, 2, 1))
+  
+  # low fragmentation
+  plot(
+    dd[[paste0("low_", samp)]],
+    col = cols["low"]
+  )
+  mtext("Low fragmentation", side = 3, line = 0.3, cex = 0.9)
+  mtext(
+    paste("Sampling method:", samp),
+    side = 1, line = 3, cex = 0.8, col = "grey30"
+  )
+  
+  # high fragmentation
+  plot(
+    dd[[paste0("high_", samp)]],
+    col = cols["high"]
+  )
+  mtext("High fragmentation", side = 3, line = 0.3, cex = 0.9)
+  mtext(
+    paste("Sampling method:", samp),
+    side = 1, line = 3, cex = 0.8, col = "grey30"
+  )
+}
 
 

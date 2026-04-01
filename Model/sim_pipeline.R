0eab9cbe3b2e26ccf873e0a4cb17e5a20daec7ef
Julian Nico Thiem <julianthiem@gmail.com>
2026-04-01 17:29:35 +0200

    Refactor simulation pipeline to require array job index and remove local loop execution

--- Model/sim_pipeline.R
+++ Model/sim_pipeline.R
@@ -52,7 +52,6 @@ for (method in sampling_methods) {
       format = "long"
     )
     sampled_data[[method]][[step]] <- sampled
-    print(head(sampled))
   }
 }
 
@@ -83,7 +82,7 @@ data.table::fwrite(
   na = "NA"
 )
 
-# Multiple runs in a loop ------------------------------------------------
+# Multiple runs in array job ------------------------------------------------
 
 # For each run, we need to build a var_par with varied ac and frag values,
 # to get all combinations of the two parameters.
@@ -102,117 +101,61 @@ var_par_df <- tidyr::expand_grid(
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
-
-  for (i in seq_len(nrow(var_par_df))) {
-    
-    sim_id <- i
-    cat("Running simulation ", sim_id, " with ac = ", var_par_df$ac[i], " and frag = ", var_par_df$frag[i], "\n")
-    var_par <- var_par_df[i, ]
-
-    results <- clean_run(
-      mod_par = mod_par,
-      var_par = var_par,
-      switch = switch,
-      sim_id = sim_id,
-      seed = master_seed + i, # Use a different seed for each run
-      record_steps = c("start", "pre_fragmentation", "post_fragmentation", "final")
-    )
-
-    recorded_steps <- names(results)
-    sampling_methods <- "random"
-    for (method in sampling_methods) {
-      sampled <- list()
-      for (step in recorded_steps) {
-        sampled[[step]] <- sample_cells(
-          results[[step]],
-          method = method,
-          n_samples = 30,
-          format = "long"
-        )
-      }
-      
-      sampled_df <- rbindlist(sampled)
-      
-      sampled_df <- sampled_df %>%
-        tidyr::pivot_wider(names_from  = species_id,
-                          values_from = n,
-                          values_fill = 0,
-                          names_prefix = "sp_",
-                          names_sort = TRUE)
-      
-      if ("sp_NA" %in% colnames(sampled_df)) {
-        sampled_df <- sampled_df %>%
-          select(-sp_NA)
-      }
-      
-      # Export to CSV
-      data.table::fwrite(
-        sampled_df, 
-        file = paste0(here("data-raw/sampled_data/"), "sim_", sim_id, "_", 
-                      "ac_", var_par$ac, 
-                      "_frag_", var_par$frag, "_", 
-                      "samp_", method, ".csv"),
-        na = "NA"
-      )
-    }
-
-    cat("\n")
-  }
-
-} else {
-
-  index <- as.numeric(args[1])
-  sim_id <- index
-  cat("Running simulation ", sim_id, " with ac = ", var_par_df$ac[index], " and frag = ", var_par_df$frag[index], "\n")
-  var_par <- var_par_df[index, ]
-
-  results <- clean_run(
-    mod_par = mod_par,
-    var_par = var_par,
-    switch = switch,
-    sim_id = sim_id,
-    seed = master_seed + index, # Use a different seed for each run
-    record_steps = c("start", "pre_fragmentation", "post_fragmentation", "final")
-  )
-
-  recorded_steps <- names(results)
-  sampling_methods <- "random"
-  for (method in sampling_methods) {
-    sampled <- list()
-    for (step in recorded_steps) {
-      sampled[[step]] <- sample_cells(
-        results[[step]],
-        method = method,
-        n_samples = 30,
-        format = "long"
-      )
-    }
-    
-    sampled_df <- rbindlist(sampled)
-    
-    sampled_df <- sampled_df %>%
-      tidyr::pivot_wider(names_from  = species_id,
-                         values_from = n,
-                         values_fill = 0,
-                         names_prefix = "sp_",
-                         names_sort = TRUE)
-    
-    if ("sp_NA" %in% colnames(sampled_df)) {
-      sampled_df <- sampled_df %>%
-        select(-sp_NA)
-    }
-    
-    # Export to CSV
-    data.table::fwrite(
-      sampled_df, 
-      file = paste0(here("data-raw/sampled_data/"), "sim_", sim_id, "_", 
-                    "ac_", var_par$ac, 
-                    "_frag_", var_par$frag, "_", 
-                    "samp_", method, ".csv"),
-      na = "NA"
-    )
-  }
-
-  cat("\n")
+  stop("No index provided. Please provide an index for the parameter combination to run.")
 }
+
+index <- as.numeric(args[1])
+sim_id <- index
+cat("Running simulation", sim_id, "with ac =", var_par_df$ac[index], "and frag =", var_par_df$frag[index], "\n")
+var_par <- var_par_df[index, ]
+
+results <- clean_run(
+  mod_par = mod_par,
+  var_par = var_par,
+  switch = switch,
+  sim_id = sim_id,
+  seed = master_seed + index, # Use a different seed for each run
+  record_steps = c("start", "pre_fragmentation", "post_fragmentation", "final")
+)
+
+recorded_steps <- names(results)
+sampling_methods <- "random"
+for (method in sampling_methods) {
+  sampled <- list()
+  for (step in recorded_steps) {
+    sampled[[step]] <- sample_cells(
+      results[[step]],
+      method = method,
+      n_samples = 30,
+      format = "long"
+    )
+  }
+  
+  sampled_df <- rbindlist(sampled)
+  
+  sampled_df <- sampled_df %>%
+    tidyr::pivot_wider(names_from  = species_id,
+                        values_from = n,
+                        values_fill = 0,
+                        names_prefix = "sp_",
+                        names_sort = TRUE)
+  
+  if ("sp_NA" %in% colnames(sampled_df)) {
+    sampled_df <- sampled_df %>%
+      select(-sp_NA)
+  }
+  
+  # Export to CSV
+  data.table::fwrite(
+    sampled_df, 
+    file = paste0(here("data-raw/sampled_data/"), "sim_", sim_id, "_", 
+                  "ac_", var_par$ac, 
+                  "_frag_", var_par$frag, "_", 
+                  "samp_", method, ".csv"),
+    na = "NA"
+  )
+}
+
+cat("\n")
+
 

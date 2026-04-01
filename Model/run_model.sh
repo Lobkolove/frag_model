7ef16d468a0c4ebc3ac25f26e17b1742bb4480fe
Julian Nico Thiem <julianthiem@gmail.com>
2026-04-01 11:52:30 +0200

    Update SLURM configuration and script parameters for single task execution

--- Model/run_model.sh
+++ Model/run_model.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
 #SBATCH --job-name=frag_red_hab
-#SBATCH --mail-user=
+#SBATCH --mail-user=j.n.thiem@fu-berlin.de
 #SBATCH --mail-type=end
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=700
-#SBATCH --time=2:00:00
+#SBATCH --time=00:05:00
 #SBATCH --qos=standard
-#SBATCH --array=1-10
 
 module add R
 
-Rscript "cluster_model_run.R" ${SLURM_ARRAY_TASK_ID} ${SLURM_CPUS_PER_TASK}
+Rscript "sim_pipeline.R"

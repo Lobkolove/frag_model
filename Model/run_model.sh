64870ae90c2a51ddbca90608cda208b63674c0dc
J. N. Thiem <julianthiem@gmail.com>
2026-04-03 20:48:07 +0200

    Update SBATCH time limit to 2 hours in run_model.sh

--- Model/run_model.sh
+++ Model/run_model.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=700
-#SBATCH --time=01:40:00
+#SBATCH --time=02:00:00
 #SBATCH --qos=standard
 #SBATCH --output=logs/frag_seq_test_%j_%a.out
 #SBATCH --array=16-30

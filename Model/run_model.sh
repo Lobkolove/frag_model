a142bf747c979e6969ad0c9c96053e0cdf54f833
J. N. Thiem <julianthiem@gmail.com>
2026-04-17 09:03:50 +0200

    Increase SLURM job time limit to 3 hours in run_model.sh

--- Model/run_model.sh
+++ Model/run_model.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=700
-#SBATCH --time=02:00:00
+#SBATCH --time=03:00:00
 #SBATCH --qos=standard
 #SBATCH --output=logs/frag_sim_%A_%a.out
 #SBATCH --error=logs/frag_sim_%A_%a.err

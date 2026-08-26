4fbc0cfa6c8db30dcf0100ee20a073cc9ad07e4c
Thiem <thiej93@login.curta.zedat.fu-berlin.de>
2026-08-26 09:10:21 +0200

    Change parameters and batch script for random habitat dispersal series

--- Model/run_model.sh
+++ Model/run_model.sh
@@ -7,11 +7,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=700
-#SBATCH --time=03:00:00
+#SBATCH --time=03:20:00
 #SBATCH --qos=standard
 #SBATCH --output=logs/frag_sim_%A_%a.out
 #SBATCH --error=logs/frag_sim_%A_%a.err
-#SBATCH --array=1-9
+#SBATCH --array=1-30
 
 module add R
 module add GDAL
